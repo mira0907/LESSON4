16270d3c2b11b8e535bf726e181088ef080b79c2
mira0907 <87653245+mira0907@users.noreply.github.com>
2021-07-23 09:53:41 +0800

    Add files via upload

---
+++ lesson 4 HW.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 22 11:47:22 2021
+
+@author: dkjua
+"""
+for b  in range(11):
+    print(b*'*')
---
+++ lesson 4-2 HW.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 22 12:08:00 2021
+
+@author: dkjua
+"""
+for b  in range(4,0,-1 ):
+    print(b*'*')
+    
